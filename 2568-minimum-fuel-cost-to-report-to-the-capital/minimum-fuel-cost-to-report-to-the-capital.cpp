d0f82d5f430613db239a97177af7d79613946667
Tinkal kumar <tinkal300@gmail.com>
2025-01-11 13:42:27 +0530

    Time: 971 ms (6.76%) | Memory: 195.8 MB (28.17%) - LeetSync

--- 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -11,25 +11,25 @@ public:
     }
 
     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-        int n = roads.size() + 1;
-        if (n == 1) return 0;
+        int n = roads.size() ;
+        if (n == 0) return 0;
 
-        vector<vector<int>> adj(n);
+        vector<vector<int>> adj(n+1);
         for (auto it : roads) {
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
 
-        vector<int> subtreeCount(n, 0);
+        vector<int> subtreeCount(n+1, 0);
         dfs(0, -1, adj, subtreeCount);
 
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n+1; ++i) {
             cout << subtreeCount[i] << ' ';
         }
      long long ans = 0;
-         for(int i=1;i<n;i++){
+         for(int i=1;i<n+1;i++){
             ans += ceil((double)subtreeCount[i]/seats);
          }
-        return ans; // Placeholder for fuel cost calculation
+        return ans; 
     }
 };
