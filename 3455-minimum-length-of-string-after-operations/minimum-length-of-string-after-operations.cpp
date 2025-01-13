c1ef2008f798b1c5e21286b27135f79cf2379182
Tinkal kumar <tinkal300@gmail.com>
2025-01-13 11:47:19 +0530

    Time: 7 ms (93.55%) | Memory: 32 MB (86.69%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minimumLength(string s) {
+         ios::sync_with_stdio(0); cin.tie(0);
         vector<int>freq(26,0);
         for(auto it:s)  freq[it - 'a']++;
         int ans = 0;
