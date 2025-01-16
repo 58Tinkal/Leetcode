01fe7d5ad375eb0676caf2486ca75916b4298d59
Tinkal kumar <tinkal300@gmail.com>
2025-01-16 18:31:15 +0530

    Time: 0 ms (100.00%) | Memory: 64.5 MB (12.20%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -4,10 +4,15 @@ public:
         int ans = 0;
         int n = nums1.size();
         int m = nums2.size();
-        if(n%2 == 0 && m%2 ==0) return ans;
-        for(auto it : nums1) if(m%2 == 1) ans ^= it;
-        for(auto it : nums2) if(n%2 == 1) ans ^= it;
-        cout<<ans<<endl;
+        if (n % 2 == 0 && m % 2 == 0)
+            return ans;
+        for (auto it : nums1)
+            if (m % 2 == 1)
+                ans ^= it;
+        for (auto it : nums2)
+            if (n % 2 == 1)
+                ans ^= it;
+        cout << ans << endl;
         return ans;
     }
 };
