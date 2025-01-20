7e0a08e6dc95d7003d16e0e0fd78b130927f2db5
Tinkal kumar <tinkal300@gmail.com>
2025-01-20 19:51:07 +0530

    Time: 14 ms (81.40%) | Memory: 133.9 MB (86.89%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -3,19 +3,20 @@ public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        vector<pair<int,int>>pos(m*n+1);
-        vector<int>row(n,0),col(m,0);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                pos[mat[i][j]] = make_pair(i,j);
+        vector<pair<int, int>> pos(m * n + 1);
+        vector<int> row(n, 0), col(m, 0);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                pos[mat[i][j]] = make_pair(i, j);
             }
         }
-        for(int i=0;i<arr.size();i++){
+        for (int i = 0; i < arr.size(); i++) {
             auto it = pos[arr[i]];
             row[it.second]++;
             col[it.first]++;
-            if(row[it.second] == m || col[it.first] == n) return i; 
+            if (row[it.second] == m || col[it.first] == n)
+                return i;
         }
-        return arr.size()-1;
+        return arr.size() - 1;
     }
 };
