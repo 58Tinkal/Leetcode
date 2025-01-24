467c048c0854fd6c1e0a63020eb9b265527b484c
Tinkal kumar <tinkal300@gmail.com>
2025-01-24 23:39:34 +0530

    Time: 114 ms (25.31%) | Memory: 132 MB (77.05%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,39 +1,69 @@
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
 class Solution {
 public:
-    int dfs(int node,int &ans,vector<vector<pair<int,int>>>&adj,vector<int>&vis){
-        vis[node] = 1;
-             for(auto it:adj[node]){
-                ans = min(ans,it.second);
-                if(vis[it.first] == 0){
-                    vis[it.first] = 1;
-                    dfs(it.first,ans,adj,vis);
-                }
-            }
-            return ans;
-    }
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>>adj(n+1);
+        DisjointSet ds(n+1);
         for(auto it:roads){
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});
+            ds.unionBySize(it[0],it[1]);
         }
-        // queue<int>q;
-        // q.push(1);
-         vector<int>vis(n+1,0);
-        // vis[1] = 1;
         int ans = INT_MAX;
-        // while(!q.empty()){
-        //     auto node = q.front();
-        //     q.pop();
-        //     for(auto it:adj[node]){
-        //         ans = min(ans,it.second);
-        //         if(vis[it.first] == 0){
-        //             vis[it.first] = 1;
-        //             q.push(it.first);
-        //         }
-        //     }
-        // }/
-        dfs(1,ans,adj,vis);
+        for(auto it:roads){
+          int a = ds.findUPar(1);
+          int b = ds.findUPar(it[0]);
+          int c = ds.findUPar(it[1]);
+          if(a == b && a == c ){
+            ans = min(ans,it[2]);
+          }
+        }
         return ans;
     }
 };
