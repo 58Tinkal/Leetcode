719a09de253e2345d066f2ffe23a5eadef8e5c4a
Tinkal kumar <tinkal300@gmail.com>
2025-01-24 23:33:57 +0530

    Time: 108 ms (33.66%) | Memory: 146 MB (22.53%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
+    int dfs(int node,int &ans,vector<vector<pair<int,int>>>&adj,vector<int>&vis){
+        vis[node] = 1;
+             for(auto it:adj[node]){
+                ans = min(ans,it.second);
+                if(vis[it.first] == 0){
+                    vis[it.first] = 1;
+                    dfs(it.first,ans,adj,vis);
+                }
+            }
+            return ans;
+    }
     int minScore(int n, vector<vector<int>>& roads) {
         vector<vector<pair<int,int>>>adj(n+1);
         for(auto it:roads){
             adj[it[0]].push_back({it[1],it[2]});
             adj[it[1]].push_back({it[0],it[2]});
         }
-        queue<int>q;
-        q.push(1);
-        vector<int>vis(n+1,0);
-        vis[1] = 1;
+        // queue<int>q;
+        // q.push(1);
+         vector<int>vis(n+1,0);
+        // vis[1] = 1;
         int ans = INT_MAX;
-        while(!q.empty()){
-            auto node = q.front();
-            q.pop();
-            for(auto it:adj[node]){
-                ans = min(ans,it.second);
-                if(vis[it.first] == 0){
-                    vis[it.first] = 1;
-                    q.push(it.first);
-                }
-            }
-        }
+        // while(!q.empty()){
+        //     auto node = q.front();
+        //     q.pop();
+        //     for(auto it:adj[node]){
+        //         ans = min(ans,it.second);
+        //         if(vis[it.first] == 0){
+        //             vis[it.first] = 1;
+        //             q.push(it.first);
+        //         }
+        //     }
+        // }/
+        dfs(1,ans,adj,vis);
         return ans;
     }
 };
