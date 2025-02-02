81414db656ee5b42d162f1b2832277501d292c91
Tinkal kumar <tinkal300@gmail.com>
2025-02-02 15:17:04 +0530

    Time: 44 ms (81.82%) | Memory: 38 MB (18.18%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
@@ -13,8 +13,50 @@ public:
                 w++;
             if (str[i] == 'S')
                 s++;
-           x = min(i+1,abs(n-s)+abs(e-w)+2*k);
-           ans=max(ans,x);
+           int a=n,b=s,c=e,d=w,K=k;
+           if(a>=b){
+               if(b>=K){
+                 a+=K;
+                  b-=K;
+                  K=0;
+               }else{
+                  a+=b;
+                 K-=b;
+                 b=0;
+               }
+           }else{
+                if(a>=K){
+                   b+=K;
+                   a-=K;
+                   K=0;
+                }else{
+                   b+=a;
+                   K-=a;
+                   a=0;
+                }
+           }
+           if(c>=d){
+               if(d>=K){
+                  c+=K;
+                  d-=K;
+                  K=0;
+               }else{
+                  c+=d;
+                  K-=d;
+                  d=0;
+               }
+           }else{
+               if(c>=K){
+                  d+=K;
+                  c-=K;
+                  K=0;
+               }else{
+                  d+=c;
+                  K-=c;
+                  c=0;
+               }
+           }
+           ans = max(ans,abs(a-b)+abs(c-d));
         }
         return ans;
     }
