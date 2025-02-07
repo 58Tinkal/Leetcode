184a12139c2ddc13e7f3b5edd5608be93057266b
Tinkal kumar <tinkal300@gmail.com>
2025-02-07 16:59:00 +0530

    Time: 304 ms (8.36%) | Memory: 174.8 MB (11.25%) - LeetSync

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        //set<pair<int,int>>s;
+        map<int,int>m,s;
+        vector<int>ans;
+        for(auto it:queries){
+            if(m[it[0]] > 0){
+                auto itt = m[it[0]];
+              //  s.erase(itt);
+                if(s[itt] == 1)  s.erase(itt); 
+                else  s[itt]--;
+                m[it[0]] = it[1];
+                s[it[1]]++;
+            }else{
+                m[it[0]] = it[1];
+               s[it[1]]++;
+            }
+            ans.push_back(min(m.size(),s.size()));
+        }
+        return ans;
+    }
+};
