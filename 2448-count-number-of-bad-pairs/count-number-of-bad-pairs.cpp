b464f907b86c5d892dbb124fb28701cdb609a4c3
Tinkal kumar <tinkal300@gmail.com>
2025-02-09 14:14:39 +0530

    Time: 145 ms (6.08%) | Memory: 89.4 MB (39.21%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
-        for(int i = 0;i<nums.size();i++){
+        for (int i = 0; i < nums.size(); i++) {
             nums[i] -= i;
         }
-        long  long n = nums.size();
-        map<long  long,long  long>m;
-        long  long x = 0;
-        for(auto it:nums){
-           if(m.find(it) != m.end()){
-            x+=m[it];
-           }
+        long long n = nums.size();
+        map<long long, long long> m;
+        long long x = 0;
+        for (auto it : nums) {
+            if (m.find(it) != m.end()) {
+                x += m[it];
+            }
             m[it]++;
         }
-        long  long ans = n*(n-1)/2 - x;
+        long long ans = n * (n - 1) / 2 - x;
         return ans;
     }
 };
