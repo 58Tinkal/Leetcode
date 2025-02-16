d4c45fb4513e11b3d0a0c6c2aaf1206e12314a82
Tinkal kumar <tinkal300@gmail.com>
2025-02-16 17:08:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (87.88%) - LeetSync

---
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool solve(int idx, int n, vector<int>& ans, vector<bool>& vis) {
+        if (idx >= ans.size()) return true;
+        if (ans[idx] != -1) return solve(idx + 1, n, ans, vis);
+
+        for (int i = n; i >= 1; i--) {
+            if (vis[i]) continue; // Skip if already used
+            vis[i] = true;
+            ans[idx] = i;
+
+            if (i == 1) { // If placing 1, move to next position
+                if (solve(idx + 1, n, ans, vis)) return true;
+            } else if (idx + i < ans.size() && ans[idx + i] == -1) { 
+                ans[idx + i] = i;
+                if (solve(idx + 1, n, ans, vis)) return true;
+                ans[idx + i] = -1; // Backtrack
+            }
+            
+            ans[idx] = -1; // Backtrack
+            vis[i] = false;
+        }
+        return false;
+    }
+
+    vector<int> constructDistancedSequence(int n) {
+        vector<int> ans(2 * n - 1, -1);
+        vector<bool> vis(n + 1, false);
+        solve(0, n, ans, vis);
+        return ans;
+    }
+};
