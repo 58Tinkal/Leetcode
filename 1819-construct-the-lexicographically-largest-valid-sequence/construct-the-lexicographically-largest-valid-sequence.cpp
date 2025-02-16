ac5950ee16b7c88f395c88aa1f20397aad28e5b0
Tinkal kumar <tinkal300@gmail.com>
2025-02-16 17:09:03 +0530

    Time: 2 ms (33.33%) | Memory: 8.4 MB (87.88%) - LeetSync

--- 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     bool solve(int idx, int n, vector<int>& ans, vector<bool>& vis) {
-        if (idx >= ans.size()) return true;
-        if (ans[idx] != -1) return solve(idx + 1, n, ans, vis);
+        if (idx >= ans.size())
+            return true;
+        if (ans[idx] != -1)
+            return solve(idx + 1, n, ans, vis);
 
         for (int i = n; i >= 1; i--) {
-            if (vis[i]) continue; // Skip if already used
+            if (vis[i])
+                continue;
             vis[i] = true;
             ans[idx] = i;
 
-            if (i == 1) { // If placing 1, move to next position
-                if (solve(idx + 1, n, ans, vis)) return true;
-            } else if (idx + i < ans.size() && ans[idx + i] == -1) { 
+            if (i == 1) {
+                if (solve(idx + 1, n, ans, vis))
+                    return true;
+            } else if (idx + i < ans.size() && ans[idx + i] == -1) {
                 ans[idx + i] = i;
-                if (solve(idx + 1, n, ans, vis)) return true;
-                ans[idx + i] = -1; // Backtrack
+                if (solve(idx + 1, n, ans, vis))
+                    return true;
+                ans[idx + i] = -1;
             }
-            
-            ans[idx] = -1; // Backtrack
+
+            ans[idx] = -1;
             vis[i] = false;
         }
         return false;
