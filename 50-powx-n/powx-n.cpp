27a2137582c6e2be96f144d23a41690f7477d413
Tinkal kumar <tinkal300@gmail.com>
2025-02-18 00:22:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.69%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    double solve(double x, long long n) {  // Use long long to handle INT_MIN safely
+    double solve(double x, long long n) {
         if (n == 0)
             return 1;
         double t = solve(x, n / 2);
@@ -9,7 +9,7 @@ public:
         return t * t;
     }
     double myPow(double x, int n) {
-        long long N = n; // Convert n to long long to avoid overflow
+        long long N = n;
         return (N < 0) ? solve(1 / x, -N) : solve(x, N);
     }
 };
