b039c2e1776a9b6b455c4f9df334af526ab0b769
Tinkal kumar <tinkal300@gmail.com>
2025-02-17 17:17:37 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (64.85%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -4,12 +4,14 @@ public:
     vector<bool>vis;
     bool solve(int idx,vector<int>& nums,int k,int s){
         if(k==0) return true;
-        if(s==sum) return solve(0,nums,k-1,0);
+        if(s==sum) return solve(0,nums,k-1,s=0);
         for(int i=idx;i<nums.size();i++){
             if(vis[i] || s+nums[i]>sum)  continue;
             vis[i] = true;
+           // s+=nums[i];
             if(solve(i+1,nums,k,s+nums[i])) return true;
             vis[i] = false;
+          //  s-=nums[i];
             if (s == 0) return false;
         }
         return false;
@@ -22,6 +24,7 @@ public:
         }
         sum = sum/k;
         vis = vector<bool>(nums.size(),false);
+      
         return solve(0,nums,k,0);
     }
 };
