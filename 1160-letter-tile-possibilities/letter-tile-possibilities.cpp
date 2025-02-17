ae6e78b6ae8e099002d988d1287cb33d60a6dec5
Tinkal kumar <tinkal300@gmail.com>
2025-02-17 15:22:14 +0530

    Time: 6 ms (54.86%) | Memory: 9 MB (59.19%) - LeetSync

---
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+   int ans = 0;
+   void solve(string &tiles, vector<bool> &visited, string s){
+          //if(idx>=tiles.size()) return;
+       for(int i=0;i<tiles.size();i++){
+           if (visited[i]) continue;
+            if (i > 0 && tiles[i] == tiles[i - 1] && !visited[i - 1]) continue; 
+            visited[i] = true;
+            ans++;
+            solve(tiles, visited, s + tiles[i]);
+            visited[i] = false;
+       }
+   }
+    int numTilePossibilities(string tiles) {
+         sort(tiles.begin(), tiles.end()); 
+        vector<bool> visited(tiles.size(), false);
+        solve(tiles, visited, "");
+        return ans;
+    }
+};
