d7f906e8e1f69d472c6d9ede1924c030a69fcf49
Tinkal kumar <tinkal300@gmail.com>
2025-02-17 17:04:54 +0530

    Time: 4 ms (82.52%) | Memory: 11.7 MB (94.62%) - LeetSync

---
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int sum=0;
+    vector<bool>vis;
+    bool solve(int idx,vector<int>& nums,int k,int s){
+        if(k==0) return true;
+        if(s==sum) return solve(0,nums,k-1,0);
+        for(int i=idx;i<nums.size();i++){
+            if(vis[i] || s+nums[i]>sum)  continue;
+            vis[i] = true;
+            if(solve(i+1,nums,k,s+nums[i])) return true;
+            vis[i] = false;
+            if (s == 0) return false;
+        }
+        return false;
+    }
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+      
+        for(auto &it:nums) sum+=it;
+        if(sum%k != 0 || nums.size() < k){
+            return false;
+        }
+        sum = sum/k;
+        vis = vector<bool>(nums.size(),false);
+        return solve(0,nums,k,0);
+    }
+};
