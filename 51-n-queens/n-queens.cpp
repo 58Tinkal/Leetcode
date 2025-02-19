a053f6bbc123b9daf91b52362315d06f7da02d01
Tinkal kumar <tinkal300@gmail.com>
2025-02-19 12:09:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (36.10%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,61 +1,32 @@
 class Solution {
 public:
-    // bool isSafe(int row,int col,vector<string>&ds,int n){
-    //     int duprow=row;
-    //     int dupcol=col;
-    //     while(row>=0&&col>=0){
-    //        if(ds[row][col]=='Q') return false;
-    //        row--;
-    //        col--;
-    //     }
-    //     row=duprow;
-    //     col=dupcol;
-    //      while(col>=0){
-    //        if(ds[row][col]=='Q') return false;
-    //        col--;
-    //     }
-    //       row=duprow;
-    //     col=dupcol;
-    //     while(row<n&&col>=0){
-    //          if(ds[row][col]=='Q') return false;
-    //        col--;
-    //        row++;
-    //     }
-    //     return true;
-    // }
-    void fillQueen(int col, int n, vector<vector<string>>& ans,
-                   vector<string>& ds, vector<int>& leftRow,
-                   vector<int>& upperDio, vector<int>& lowerDio) {
-        if (col == n) {
+    vector<int> col, ld, rd;
+    vector<string> ds;
+    vector<vector<string>> ans;
+
+    void solve(int r, int n) {
+        if (r == n) {
             ans.push_back(ds);
             return;
         }
-        for (int row = 0; row < n; row++) {
-            if (leftRow[row] == 0 && upperDio[n - 1 - row + col] == 0 &&
-                lowerDio[row + col] == 0) {
-
-                ds[row][col] = 'Q';
-                leftRow[row] = 1;
-                lowerDio[row + col] = 1;
-                upperDio[n - 1 - row + col] = 1;
-                fillQueen(col + 1, n, ans, ds, leftRow, upperDio, lowerDio);
-                ds[row][col] = '.';
-                leftRow[row] = 0;
-                lowerDio[row + col] = 0;
-                upperDio[n - 1 - row + col] = 0;
-            }
-        } 
-    }
-    vector<vector<string>> solveNQueens(int n) {
-        vector<string> ds(n);
-        vector<vector<string>> ans;
-        // string s(n,'.');
         for (int i = 0; i < n; i++) {
-            ds[i] = string(n, '.');
+            if (col[i] == 0 && ld[r - i + n - 1] == 0 && rd[r + i] == 0) {
+                ds[r][i] = 'Q';
+                col[i] = ld[r - i + n - 1] = rd[r + i] = 1;
+                solve(r + 1, n);
+                ds[r][i] = '.';
+                col[i] = ld[r - i + n - 1] = rd[r + i] = 0;
+            }
         }
-        vector<int> leftRow(n, 0), upperDio(2 * n - 1, 0),
-            lowerDio(2 * n - 1, 0);
-        fillQueen(0, n, ans, ds, leftRow, upperDio, lowerDio);
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        ds = vector<string>(n, string(n, '.'));
+        ans.clear(); 
+        col = vector<int>(n, 0);
+        ld = vector<int>(2 * n - 1, 0);
+        rd = vector<int>(2 * n - 1, 0);
+        solve(0, n);
         return ans;
     }
 };
