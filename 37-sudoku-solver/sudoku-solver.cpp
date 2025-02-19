0c32837dffac67e533823be6ec158765523da4eb
Tinkal kumar <tinkal300@gmail.com>
2025-02-19 14:50:28 +0530

    Time: 425 ms (31.05%) | Memory: 8.9 MB (11.27%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
    bool isValid(vector<vector<char>>& board, int row, int col, char ch) {
        for (int i = 0; i < 9; i++) {
-           if (board[i][col] == ch) return false; // Check column
-           if (board[row][i] == ch) return false; // Check row
-           if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == ch) return false; // Check 3x3 box
+           if (board[i][col] == ch) return false; 
+           if (board[row][i] == ch) return false; 
+           if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == ch) return false; 
        }
        return true;
    }
