10d331a854ee9da18834eb945ac9f9fda6824db5
Tinkal kumar <tinkal300@gmail.com>
2025-02-20 15:00:13 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (29.76%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    set<string>s;
+    int n;
+    string ans;
+    bool solve(int idx,string temp){
+        if(idx == n){
+            if(s.find(temp) == s.end()){
+                ans = temp;
+                return true;
+            }
+            return false;
+        }
+        return solve(idx+1,temp+"1")||solve(idx+1,temp+"0");
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        s = set<string>(nums.begin(),nums.end());
+        n = nums[0].size();
+        string temp = "";
+        if(solve(0,temp)) return ans;
+        // solve(0, temp);
+         return " ";
+    }
+};
