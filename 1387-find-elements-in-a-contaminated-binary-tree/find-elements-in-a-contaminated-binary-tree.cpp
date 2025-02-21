e946489f69b6422c88adf59a6cf0ad4381a85a6c
Tinkal kumar <tinkal300@gmail.com>
2025-02-21 16:54:07 +0530

    Time: 319 ms (18.51%) | Memory: 25.6 MB (96.82%) - LeetSync

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    TreeNode* recoveredRoot;
+    FindElements(TreeNode* root) {
+        recoveredRoot = root;
+        if (root) {
+            root->val = 0;
+            solve(root);
+        }
+    }
+
+    void solve(TreeNode* root) {
+        if (!root)
+            return;
+        if (root->left) {
+            root->left->val = 2 * root->val + 1;
+            solve(root->left);
+        }
+        if (root->right) {
+            root->right->val = 2 * root->val + 2;
+            solve(root->right);
+        }
+    }
+    bool findE(TreeNode* root, int t) {
+        if (root->val == t)
+            return true;
+        if (root == NULL)
+            return false;
+        bool f1 = false;
+        bool f2 = false;
+        if (root->left)
+            f1 = findE(root->left, t);
+        if (root->right)
+            f2 = findE(root->right, t);
+        return f1 || f2;
+    }
+    bool find(int target) {
+        if (findE(recoveredRoot, target))
+            return true;
+        return false;
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
