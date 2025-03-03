b6d16d33a0185e6d3cfa1b9fdff4b840799b369f
Tinkal kumar <tinkal300@gmail.com>
2025-03-03 23:40:48 +0530

    Time: 30 ms (80.27%) | Memory: 101.2 MB (78.15%) - LeetSync

--- 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.cpp
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.cpp
@@ -1,32 +1,37 @@
+#define ll long long int
 class Solution {
 public:
-
     long long maxPoints(vector<vector<int>>& p) {
-        int n=p.size();
-        int m=p[0].size();
-        vector<long long>dp;
-        for(int i=0;i<m;i++)dp.push_back(p[0][i]);
-        for(int i=1;i<n;i++){
-            vector<long long>dp2(m);
-        long long mx=dp[0];
-            for(int j=0;j<m;j++){
-                 dp2[j]=max(mx,dp[j]);
-                 mx=dp2[j]-1;
+        int n = p.size();
+        int m = p[0].size();
+        vector<ll> dp(p[0].begin(), p[0].end());
+        
+        for (int i = 1; i < n; i++) {
+            vector<ll> temp(m);
+
+            // Left to right traversal
+            ll mx = dp[0];
+            for (int j = 0; j < m; j++) {
+                mx = max(mx, dp[j]);
+                temp[j] = mx;
+                mx--;  
             }
-            mx=dp[m-1];
-              for(int j=m-1;j>=0;j--){
-                 dp2[j]=max(dp2[j],max(mx,dp[j]));
-                 mx=dp2[j]-1;
+
+            // Right to left traversal
+            mx = dp[m - 1];
+            for (int j = m - 1; j >= 0; j--) {
+                mx = max(mx, dp[j]);  
+                temp[j] = max(temp[j], mx); 
+                mx--;  
             }
-            for(int j=0;j<m;j++){
-                dp2[j]+=p[i][j];
+
+            // Update dp with the current row values
+            for (int j = 0; j < m; j++) {
+                dp[j] = temp[j] + p[i][j];
             }
-          dp=dp2;
         }
-    long long mx=dp[0];
-        for(int i=0;i<m;i++){
-            mx=max(dp[i],mx);
-        }
-        return mx;
+
+  
+        return *max_element(dp.begin(), dp.end());
     }
 };
