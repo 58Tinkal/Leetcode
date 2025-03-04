75608823d94830015db0ac75ec51b52731ca7ecc
Tinkal kumar <tinkal300@gmail.com>
2025-03-04 18:18:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (67.84%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-string decimalToBase3(int n) {
-    if (n == 0) return "0";  // Edge case for zero
-    
-    string result = "";
+bool check(int n) {
+    if (n == 0) return true;  
     while (n > 0) {
-        result = char('0' + (n % 3)) + result;  // Prepend remainder
+        if(n%3 == 2) return false;
         n /= 3;
     }
-    return result;
+    return true;
 }
     bool checkPowersOfThree(int n) {
-        string s = decimalToBase3(n);
-        cout<<s<<endl;
-        for(auto &it:s){
-            if(it == '2') return false;
-        }
-        return true;
+        return check(n);
     }
 };
