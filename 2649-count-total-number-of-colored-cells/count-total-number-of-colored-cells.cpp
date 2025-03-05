cc789f55466eec790583ba231d3bd9209a0bdc4e
Tinkal kumar <tinkal300@gmail.com>
2025-03-05 22:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (61.42%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long x = 5;
-        if(n==1) {
-            return 1;
-        }
-        if(n==2)  return 5;
-        long long p=8;
-        for(int i=3;i<=n;i++){
-            x+=p;
-            p+=4;
-        }
-        cout<<x<<endl;
-        return x;
+        return 1LL * n * n + 1LL * (n - 1) * (n - 1);
     }
 };
