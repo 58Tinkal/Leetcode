6a991f9ced8aefd412beae840e89093b0f97a1d5
Tinkal kumar <tinkal300@gmail.com>
2025-03-08 10:55:30 +0530

    Time: 3 ms (6.58%) | Memory: 8.3 MB (62.68%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int minimumRecolors(string blocks, int k) {
-        int n = blocks.size();
-        vector<int>p(n,0);
-        for(int i=0;i<n;i++){
-            if(i==0){
-                p[i] = (blocks[i] == 'W')?1:0;
-            }else{
-                p[i] = p[i-1] + ((blocks[i] == 'W')?1:0);
-            }
+    int minimumRecolors(string s, int k) {
+        int c = 0, ans = 1e9;
+        int n = s.size();
+        for (int i = 0; i < k; i++) {
+            if (s[i] == 'W')
+                c++;
         }
-        for(int i=0;i<n;i++)  cout<<p[i]<<' ';
-        int ans = p[k-1];
-        int s=0,e=k;
-        while(e<n){
-          int x = p[e] - p[s];
-          e++; s++;
-          ans = min(ans,x);
-        } 
-        return ans; 
+        ans = c;
+        for (int i = k; i < n; i++) {
+            if (s[i - k] == 'W')
+                c--;
+            if (s[i] == 'W')
+                c++;
+            ans = min(ans, c);
+        }
+        return ans;
     }
 };
