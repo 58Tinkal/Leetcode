76de3ccc8188a86e1688a67583d61f91d26796ba
Tinkal kumar <tinkal300@gmail.com>
2025-03-15 14:18:32 +0530

    Time: 67 ms (34.25%) | Memory: 338.9 MB (36.30%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,29 +1,46 @@
 class Solution {
 public:
-    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+    bool fun(vector<int>& nums, vector<vector<int>>& q, int m) {
         int n = nums.size();
-        auto canMakeZeroArray = [&](int k) {
-            vector<int> diff(n + 1, 0);
-            for (int i = 0; i < k; i++) {
-                int left = queries[i][0], right = queries[i][1], val = queries[i][2];
-                diff[left] += val;
-                diff[right + 1] -= val;
-            }
-            int currVal = 0;
-            for (int i = 0; i < n; i++) {
-                currVal += diff[i];
-                if (currVal < nums[i]) return false;
-            }
-            return true;
-        };
-        if (all_of(nums.begin(), nums.end(), [](int x) { return x == 0; })) return 0;
-        int left = 1, right = queries.size();
-        if (!canMakeZeroArray(right)) return -1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (canMakeZeroArray(mid)) right = mid;
-            else left = mid + 1;
+        vector<int> a(n, 0); 
+
+        for (int i = 0; i < m; i++) {  
+            int l = q[i][0];
+            int r = q[i][1];
+            int v = q[i][2];
+
+            a[l] += v;
+            if (r + 1 < n)
+                a[r + 1] -= v;
         }
-        return left; 
+        for (int i = 1; i < n; i++) {  
+            a[i] += a[i - 1];
+        }
+        for (int i = 0; i < n; i++) {
+            if (a[i] < nums[i])
+                return false;
+        }
+        return true;
+    }
+
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& q) {
+        int l = 1;
+        int h = q.size() ; 
+        int ans = -1; 
+        bool f = true;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0) f = false;
+        }
+        if(f) return 0;
+        while (l <= h) {
+            int m = (l + h) / 2;
+            if (fun(nums, q, m)) {
+                ans = m;
+                h = m - 1;  
+            } else {
+                l = m + 1;
+            }
+        }
+        return ans;
     }
 };
