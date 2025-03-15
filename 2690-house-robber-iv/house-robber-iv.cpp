75070a300948806b05dc37c006d73e3de34b018b
Tinkal kumar <tinkal300@gmail.com>
2025-03-15 13:36:34 +0530

    Time: 17 ms (87.18%) | Memory: 60.7 MB (94.16%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    bool fun(vector<int>&nums,int k, int mid){
+    bool fun(vector<int>& nums, int k, int mid) {
         int c = 0;
         int n = nums.size();
         int s = -1e9;
-    for (int i = 0; i < n; i++) {
-        if (nums[i] <= mid) {
-            c++;
-            i++;  
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= mid) {
+                c++;
+                i++;
+            }
+            if (c >= k)
+                return true;
         }
-        if (c >= k) return true;
-    }
-    
-    return false;
+        return false;
     }
     int minCapability(vector<int>& nums, int k) {
-        int l = *min_element(nums.begin(),nums.end());
-        int h = *max_element(nums.begin(),nums.end());
+        int l = *min_element(nums.begin(), nums.end());
+        int h = *max_element(nums.begin(), nums.end());
         int ans = -1;
-        while(l<=h){
-            int  mid = (l+h)/2;
-            if(fun(nums,k,mid)){
-               h = mid - 1;
-               ans = mid;
-            }else{
+        while (l <= h) {
+            int mid = (l + h) / 2;
+            if (fun(nums, k, mid)) {
+                h = mid - 1;
+                ans = mid;
+            } else {
                 l = mid + 1;
             }
         }
