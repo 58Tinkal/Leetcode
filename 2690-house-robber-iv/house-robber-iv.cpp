d011e094a36c11029723961267c2d04840c92752
Tinkal kumar <tinkal300@gmail.com>
2025-03-15 13:35:49 +0530

    Time: 22 ms (68.34%) | Memory: 60.9 MB (40.42%) - LeetSync

---
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool fun(vector<int>&nums,int k, int mid){
+        int c = 0;
+        int n = nums.size();
+        int s = -1e9;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] <= mid) {
+            c++;
+            i++;  
+        }
+        if (c >= k) return true;
+    }
+    
+    return false;
+    }
+    int minCapability(vector<int>& nums, int k) {
+        int l = *min_element(nums.begin(),nums.end());
+        int h = *max_element(nums.begin(),nums.end());
+        int ans = -1;
+        while(l<=h){
+            int  mid = (l+h)/2;
+            if(fun(nums,k,mid)){
+               h = mid - 1;
+               ans = mid;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
