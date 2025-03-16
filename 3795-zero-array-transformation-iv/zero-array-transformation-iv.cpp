720e576be632d4233b57d489bb06bd79a1efbf51
Tinkal kumar <tinkal300@gmail.com>
2025-03-16 17:59:00 +0530

    Time: 39 ms (33.33%) | Memory: 46.8 MB (91.67%) - LeetSync

--- 3795-zero-array-transformation-iv/zero-array-transformation-iv.cpp
+++ 3795-zero-array-transformation-iv/zero-array-transformation-iv.cpp
@@ -1,49 +1,36 @@
 class Solution {
 public:
-    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+    bool fun(vector<int>& nums, vector<vector<int>>& q, int m) {
         int n = nums.size();
-        int m = queries.size();
-        
-        // dp[j][s] will be true if we can sum exactly s for index j using some queries
-        vector<vector<bool>> dp(n);
-        for (int j = 0; j < n; j++) {
-            dp[j].resize(nums[j] + 1, false);
-            dp[j][0] = true;
-        }
-        
-        // If nums is already a Zero Array, return 0
-        bool alreadyZero = true;
-        for (int j = 0; j < n; j++) {
-            if (nums[j] != 0) { 
-                alreadyZero = false; 
-                break; 
-            }
-        }
-        if (alreadyZero) return 0;
-        
-        // Process queries one by one
-        for (int k = 1; k <= m; k++) {
-            int l = queries[k - 1][0], r = queries[k - 1][1], v = queries[k - 1][2];
-            // For each index in the current query's range, update its dp table.
-            for (int j = l; j <= r; j++) {
-                // Update in reverse order to avoid reusing the same query multiple times
-                for (int s = nums[j] - v; s >= 0; s--) {
-                    if (dp[j][s]) {
-                        dp[j][s + v] = true;
-                    }
+        for (int i = 0; i < n; i++) {
+            int v = nums[i];
+            vector<bool> dp(v + 1, false);
+            dp[0] = true;
+            for (int j = 0; j < m; j++) {
+                if (i < q[j][0] || i > q[j][1])
+                    continue;
+                for (int k = v; k >= q[j][2]; k--) {
+                    dp[k] = dp[k] || dp[k - q[j][2]];
                 }
             }
-            // Check if all indices can sum to exactly nums[j]
-            bool allZero = true;
-            for (int j = 0; j < n; j++) {
-                if (!dp[j][nums[j]]) {
-                    allZero = false;
-                    break;
-                }
-            }
-            if (allZero) return k;
+            if (!dp[v])
+                return false;
         }
-        
-        return -1;
+        return true;
+    }
+
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& q) {
+        int totalQueries = q.size();
+        int l = 0, h = totalQueries, ans = -1;
+        while (l <= h) {
+            int m = (l + h) / 2;
+            if (fun(nums, q, m)) {
+                ans = m;
+                h = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return ans;
     }
 };
