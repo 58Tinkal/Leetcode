3b24a3d9323e4cf77b1f75b188c7af788a40798f
Tinkal kumar <tinkal300@gmail.com>
2025-03-16 12:31:52 +0530

    Time: 31 ms (69.49%) | Memory: 59.4 MB (45.76%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    bool fun(vector<int>& ranks, int cars, long long m){
+    bool fun(vector<int>& ranks, int cars, long long m) {
         long long s = -1e9;
         int x = cars;
-        for(int i=0;i<ranks.size();i++){
-           long long p = sqrt(m / ranks[i]);
-           x -= p;
-           s = max(s,p*p*ranks[i]);
-           if(x<=0 && s<=m) return true;
+        for (int i = 0; i < ranks.size(); i++) {
+            long long p = sqrt(m / ranks[i]);
+            x -= p;
+            s = max(s, p * p * ranks[i]);
+            if (x <= 0 && s <= m)
+                return true;
         }
-        return (x<=0 && s<=m);
+        return (x <= 0 && s <= m);
     }
     long long repairCars(vector<int>& ranks, int cars) {
         int n = ranks.size();
@@ -19,10 +20,10 @@ public:
         long long ans = h;
         while (l <= h) {
             long long m = l + (h - l) / 2;
-            if(fun(ranks,cars,m)){
+            if (fun(ranks, cars, m)) {
                 ans = m;
                 h = m - 1;
-            }else{
+            } else {
                 l = m + 1;
             }
         }
