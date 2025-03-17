a7e492d327429f2d6a2e506ef40c9836288f959d
Tinkal kumar <tinkal300@gmail.com>
2025-03-17 19:13:11 +0530

    Time: 3 ms (58.87%) | Memory: 9.6 MB (65.25%) - LeetSync

--- 3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.cpp
+++ 3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.cpp
@@ -2,52 +2,58 @@
 class Solution {
 public:
     ll dp[16][2];
-    ll solve(ll idx,bool tight,ll l,string &su,string &s){
+    ll solve(ll idx, bool tight, ll l, string& su, string& s) {
 
-        if(idx>=s.size()){
+        if (idx >= s.size()) {
             return 1;
         }
-        if(dp[idx][tight] != -1) return dp[idx][tight];
+        if (dp[idx][tight] != -1)
+            return dp[idx][tight];
         ll ans = 0;
         ll limit = tight ? s[idx] - '0' : 9;
-        limit = min(limit,l);
+        limit = min(limit, l);
 
         ll suIdx = s.size() - su.size();
-         // idx is in the range of sufix
-         if(idx >= suIdx   ){
+        // idx is in the range of sufix
+        if (idx >= suIdx) {
             // if tight is false then we can fil element of our choice => we can
             // make last part as sufix
-            if(tight == false) return dp[idx][tight] = 1;
+            if (tight == false)
+                return dp[idx][tight] = 1;
 
-              // we cannot be able to make last part as sufix
-            if(s[idx] < su[idx - suIdx]) return dp[idx][tight] = 0;
-             
-             // we can fill the idx noe check for others;
-            if(s[idx] == su[idx - suIdx]) return dp[idx][tight] = solve(idx+1,true,l,su,s);
-                
-                // we can match sufix
-            if(s[idx] > su[idx - suIdx]) return dp[idx][tight] = 1;
-         }
+            // we cannot be able to make last part as sufix
+            if (s[idx] < su[idx - suIdx])
+                return dp[idx][tight] = 0;
 
-        // idx is not in sufix range
-        for(int d=0;d<=limit;d++){
-            ll newTight = tight & (d == s[idx] - '0');
-            ans += solve(idx+1,newTight,l,su,s);
+            // we can fill the idx noe check for others;
+            if (s[idx] == su[idx - suIdx])
+                return dp[idx][tight] = solve(idx + 1, true, l, su, s);
+
+            // we can match sufix
+            if (s[idx] > su[idx - suIdx])
+                return dp[idx][tight] = 1;
         }
 
-       return dp[idx][tight] = ans;
+        // idx is not in sufix range
+        for (int d = 0; d <= limit; d++) {
+            ll newTight = tight & (d == s[idx] - '0');
+            ans += solve(idx + 1, newTight, l, su, s);
+        }
+
+        return dp[idx][tight] = ans;
     }
-    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
-        
+    long long numberOfPowerfulInt(long long start, long long finish, int limit,
+                                  string s) {
+
         ll x = 0;
-        string l = to_string(start-1);
-        if(l.size()>=s.size()){
+        string l = to_string(start - 1);
+        if (l.size() >= s.size()) {
             memset(dp, -1, sizeof(dp));
-            x = solve(0,1,limit,s,l);
-        } 
+            x = solve(0, 1, limit, s, l);
+        }
         string h = to_string(finish);
         memset(dp, -1, sizeof(dp));
-        ll y = solve(0,1,limit,s,h);
+        ll y = solve(0, 1, limit, s, h);
 
         return y - x;
     }
