b855e27747ec79632ec4431a48ec2ac698934f9e
Tinkal kumar <tinkal300@gmail.com>
2025-03-18 11:46:15 +0530

    Time: 2 ms (48.44%) | Memory: 60.9 MB (64.15%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
         int s = 0;
-        int length = 0;
-        int mask = 0; // To chech which bit is used
         int n = nums.size();
+        int length = 0;
+        int curr_sum = 0;
+        int curr_xor = 0;
+        // if all bits of x & y are diffrent then x & y = 0
+        // and x ^ y == x + y, We will use this property
         for (int i = 0; i < n; i++) {
-
-            while ((mask & nums[i]) != 0) {
-                // mask ^ nums[s] => flip the bit from mask which are also same
-                // as mask
-                mask ^= nums[s];
+            curr_sum += nums[i];
+            curr_xor ^= nums[i];
+            while (curr_sum != curr_xor) {
+                curr_sum -= nums[s];
+                curr_xor ^= nums[s];
                 s++;
             }
-
-            mask |= nums[i];
             length = max(length, i - s + 1);
         }
         return length;
