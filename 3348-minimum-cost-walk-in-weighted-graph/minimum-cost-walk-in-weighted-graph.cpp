f4b1d1caed66104ff9a6bad164193df78af5c2fb
Tinkal kumar <tinkal300@gmail.com>
2025-03-20 18:29:37 +0530

    Time: 336 ms (6.82%) | Memory: 477.7 MB (5.19%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    void dfs(vector<vector<pair<int, int>>>& adj, int id, int node,
-             vector<int>& vis) {
-        vis[node] = id;
-        for (auto& it : adj[node]) {
-            if (vis[it.first] == -1) { 
-                dfs(adj, id, it.first, vis);
+    void bfs(vector<vector<pair<int, int>>>&adj,int id, int node, vector<int>&vis){
+        queue<int>q;
+        q.push(node);
+        while(!q.empty()){
+            int it = q.front();
+            q.pop();
+            vis[it] = id;
+            for(auto &itt:adj[it]){
+                if(vis[itt.first] == -1){
+                    bfs(adj,id,itt.first,vis);
+                }
             }
         }
+        return;
     }
-
-    vector<int> minimumCost(int n, vector<vector<int>>& edges,
-                            vector<vector<int>>& query) {
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
         vector<int> vis(n, -1);
         vector<vector<pair<int, int>>> adjList(n);
 
@@ -19,10 +23,9 @@ public:
             adjList[edge[0]].push_back({edge[1], edge[2]});
             adjList[edge[1]].push_back({edge[0], edge[2]});
         }
-
-        for (int i = 0; i < n; i++) {
-            if (vis[i] == -1) {
-                dfs(adjList, i, i, vis);
+        for(int i = 0; i<n;i++){
+            if(vis[i] == -1){
+                bfs(adjList,i,i,vis);
             }
         }
         vector<unsigned int> cost(n, -1);
