3a46c99bed8d7d2cf4d6beca777c051ab105fccf
Tinkal kumar <tinkal300@gmail.com>
2025-03-20 18:23:20 +0530

    Time: 111 ms (43.83%) | Memory: 244.4 MB (31.82%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -1,80 +1,44 @@
-class DisjointSet {
-    vector<int> rank, parent, size;
-
-public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v)
-            return;
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-        } else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
-        } else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v)
-            return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        } else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-    }
-};
 class Solution {
 public:
+    void dfs(vector<vector<pair<int, int>>>& adj, int id, int node,
+             vector<int>& vis) {
+        vis[node] = id;
+        for (auto& it : adj[node]) {
+            if (vis[it.first] == -1) { 
+                dfs(adj, id, it.first, vis);
+            }
+        }
+    }
+
     vector<int> minimumCost(int n, vector<vector<int>>& edges,
                             vector<vector<int>>& query) {
-        DisjointSet ds(n + 1);
-        for (auto& it : edges) {
-            int u = it[0];
-            int v = it[1];
-            if (ds.findUPar(u) != ds.findUPar(v)) {
-                ds.unionBySize(u, v);
+        vector<int> vis(n, -1);
+        vector<vector<pair<int, int>>> adjList(n);
+
+        for (auto& edge : edges) {
+            adjList[edge[0]].push_back({edge[1], edge[2]});
+            adjList[edge[1]].push_back({edge[0], edge[2]});
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == -1) {
+                dfs(adjList, i, i, vis);
             }
         }
         vector<unsigned int> cost(n, -1);
         for (auto& it : edges) {
             int u = it[0];
-            int root = ds.findUPar(it[0]);
+            int root = vis[u];
             cost[root] &= it[2];
         }
         vector<int> ans;
         for (auto& it : query) {
-            int u = it[0];
-            int v = it[1];
-            if (ds.findUPar(u) != ds.findUPar(v)) {
+            if (vis[it[0]] != vis[it[1]]) {
                 ans.push_back(-1);
-                continue;
+
+            } else {
+                ans.push_back(cost[vis[it[0]]]);
             }
-            int r = ds.findUPar(u);
-            ans.push_back(cost[r]);
         }
         return ans;
     }
