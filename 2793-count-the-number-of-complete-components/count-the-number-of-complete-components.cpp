dd45c67865f55ec0b96ad40c27bc7c3e1dc3a88d
Tinkal kumar <tinkal300@gmail.com>
2025-03-22 21:07:47 +0530

    Time: 43 ms (69.89%) | Memory: 130 MB (65.96%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,74 +1,41 @@
-class DisjointSet {
-public:
-    vector<int> rank, parent, size;
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v)
-            return;
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-        } else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
-        } else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v)
-            return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        } else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-    }
-};
 class Solution {
 public:
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<int> edge(n + 1);
-        DisjointSet ds(n + 1);
-        for (auto& it : edges) {
-            int u = it[0];
-            int v = it[1];
-            ds.unionBySize(u, v);
-        }
-        for (auto it : edges) {
-            int root = ds.findUPar(it[0]);
-            edge[root]++;
-        }
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (ds.findUPar(i) == i) {
-                int k = ds.size[i];
-                int x = (k - 1) * k / 2;
-                if (x == edge[i])
-                    ans++;
+    void dfs(int node, vector<vector<int>> &adj, vector<bool> &vis, vector<int> &comp) {
+        vis[node] = true;
+        comp.push_back(node);
+        for (auto &it : adj[node]) {
+            if (!vis[it]) {
+                dfs(it, adj, vis, comp);
             }
         }
+    }
+
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        for (auto &it : edges) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+
+        int ans = 0;
+        vector<bool> vis(n, false);
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                vector<int> comp;
+                dfs(i, adj, vis, comp);
+
+                bool isComplete = true;
+                for (auto &it : comp) {
+                    if (adj[it].size() != comp.size() - 1) {
+                        isComplete = false;
+                        break;
+                    }
+                }
+                if (isComplete) ans++;
+            }
+        }
+
         return ans;
     }
 };
