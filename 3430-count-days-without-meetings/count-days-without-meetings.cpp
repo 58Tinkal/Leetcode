855bca5ff262e08582d9126e081e07db135df242
Tinkal kumar <tinkal300@gmail.com>
2025-03-24 12:09:49 +0530

    Time: 1519 ms (5.10%) | Memory: 126.6 MB (43.20%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& m) {
+        sort(m.begin(), m.end());
+        for(int i=0;i<m.size();i++){
+            cout<<m[i][0]<<' '<<m[i][1]<<endl;
+        }
+        int ans = 0;
+        int n = m.size();
+        if (m[0][0] > 1)
+            ans += m[0][0] - 1;
+            int e = -1e9;
+        for (int i = 0; i < n - 1; i++) {
+            e = max(e,m[i][1]);
+            if (e < m[i + 1][0]) {
+                ans += m[i+1][0] - e - 1;
+            }
+        }
+        e = max(e,m[n-1][1]);
+            ans += days - e;
+        return ans;
+    }
+};
