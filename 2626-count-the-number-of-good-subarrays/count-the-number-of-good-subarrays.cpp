2d14cbfdbe77f8c5e8930c98250677844d968cf6
Tinkal kumar <tinkal300@gmail.com>
2025-04-16 16:59:30 +0530

    Time: 198 ms (14.45%) | Memory: 81 MB (29.62%) - LeetSync

---
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        int n = nums.size();
+        map<long long, long long> m;
+        long long ans = 0;
+        long long cnt = 0;
+        long long l = 0;
+        for (int i = 0; i < n; i++) {
+            m[nums[i]]++;
+            if (m[nums[i]] > 1)
+                cnt += m[nums[i]] - 1;
+            if (cnt >= k)
+                ans += n - i;
+            while (cnt >= k) {
+                m[nums[l]]--;
+                if (m[nums[l]] > 0)
+                    cnt -= m[nums[l]];
+                l++;
+                if (cnt >= k)
+                    ans += n - i;
+            }
+        }
+        cout << cnt << endl;
+        if (cnt >= k)
+            ans++;
+        while (cnt >= k) {
+            if (m[nums[l]] > 0)
+                m[nums[l]]--;
+            if (m[nums[l]] > 0)
+                cnt -= m[nums[l]];
+            l++;
+            if (cnt >= k)
+                ans += 1;
+        }
+        if (cnt >= k)
+            ans++;
+        return ans;
+    }
+};
