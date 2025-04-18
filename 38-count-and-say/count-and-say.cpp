ba8421cc2e13441e80b86571be3e0229365c253c
Tinkal kumar <tinkal300@gmail.com>
2025-04-18 14:11:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (98.16%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    string fun(int n) {
-        if (n == 1) {
-            return "1";
-        }
-        string s = fun(n - 1);
-        string str = "";
-        int m = s.size();
-        char c = s[m - 1];
-        int cnt = 0;
-        for (int i = m - 1; i >= 0; i--) {
-            if (c == s[i]) {
-                cnt++;
-            } else {
-                str += c;
-                str += char(cnt + '0');
-                cnt = 1;
-                c = s[i];
+    string countAndSay(int n) {
+        string s = "1";
+        if (n == 1)
+            return s;
+        for (int i = 2; i <= n; i++) {
+            int m = s.size();
+            char c = s[m - 1];
+            int cnt = 0;
+            string str = "";
+            for (int j = m - 1; j >= 0; j--) {
+                if (c == s[j]) {
+                    cnt++;
+                } else {
+                    str += c;
+                    str += char(cnt + '0');
+                    cnt = 1;
+                    c = s[j];
+                }
             }
+            str += c;
+            str += char(cnt + '0');
+            reverse(str.begin(), str.end());
+            s = str;
         }
-        str += c;
-        str += char(cnt + '0');
-        reverse(str.begin(), str.end());
-        return str;
+        return s;
     }
-    string countAndSay(int n) { return fun(n); }
 };
