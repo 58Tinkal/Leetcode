0103d9c4f545b5f505946f9594a454db79a14237
Tinkal kumar <tinkal300@gmail.com>
2025-04-24 20:52:42 +0530

    Time: 27 ms (38.15%) | Memory: 42.2 MB (42.57%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
-        set<int>s(nums.begin(),nums.end());
+        set<int> s(nums.begin(), nums.end());
         int k = s.size();
-        map<int,int>m;
+        map<int, int> m;
         int n = nums.size();
         int st = 0;
         int ans = 0;
-        for(int i=0;i<n;i++){
-          m[nums[i]]++;
-          if(m.size() == k) ans += n-i;
-          while(m.size() >= k){
-             if(m[nums[st]] > 1){
-                m[nums[st]]--;
-             }else if(m[nums[st]] == 1){
-                m.erase(nums[st]);
-             }
-             st++;
-             if(m.size() == k) ans += n-i;
-          }
+        for (int i = 0; i < n; i++) {
+            m[nums[i]]++;
+            if (m.size() == k)
+                ans += n - i;
+            while (m.size() >= k) {
+                if (m[nums[st]] > 1) {
+                    m[nums[st]]--;
+                } else if (m[nums[st]] == 1) {
+                    m.erase(nums[st]);
+                }
+                st++;
+                if (m.size() == k)
+                    ans += n - i;
+            }
         }
         return ans;
     }
