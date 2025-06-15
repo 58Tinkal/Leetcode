be26c0bbb5a88dcb17ce0e77a8d6f53073ef97bc
Tinkal kumar <tinkal300@gmail.com>
2025-06-15 17:10:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (43.13%) - LeetSync

---
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxDiff(int num) {
+        string s = to_string(num);
+        string maxNum = s, minNum = s;
+
+        // Maximize: Replace first non-9 digit with 9
+        char toReplaceMax = 0;
+        for (char c : s) {
+            if (c != '9') {
+                toReplaceMax = c;
+                break;
+            }
+        }
+        if (toReplaceMax) {
+            for (char &c : maxNum) {
+                if (c == toReplaceMax) c = '9';
+            }
+        }
+
+        // Minimize:
+        char toReplaceMin = 0;
+        if (s[0] != '1') {
+            toReplaceMin = s[0];
+            for (char &c : minNum) {
+                if (c == toReplaceMin) c = '1';
+            }
+        } else {
+            for (int i = 1; i < s.size(); ++i) {
+                if (s[i] != '0' && s[i] != '1') {
+                    toReplaceMin = s[i];
+                    for (int j = 1; j < s.size(); ++j) {
+                        if (minNum[j] == toReplaceMin) minNum[j] = '0';
+                    }
+                    break;
+                }
+            }
+        }
+
+        int a = stoi(maxNum);
+        int b = stoi(minNum);
+        return a - b;
+    }
+};
