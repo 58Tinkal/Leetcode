0b4425770c36afedf9460eba8abcd11073edc495
Tinkal kumar <tinkal300@gmail.com>
2025-06-21 13:32:32 +0530

    Time: 291 ms (40.56%) | Memory: 342.8 MB (23.94%) - LeetSync

--- 3845-maximum-sum-of-edge-values-in-a-graph/maximum-sum-of-edge-values-in-a-graph.cpp
+++ 3845-maximum-sum-of-edge-values-in-a-graph/maximum-sum-of-edge-values-in-a-graph.cpp
@@ -1,83 +1,60 @@
 class Solution {
 public:
-    using ll = long long;
-
-    // DFS to count component size & detect a cycle
-    bool dfs(int u, int parent,
-             const vector<vector<int>>& G,
-             vector<bool>& visited,
-             int& compSize)
-    {
-        visited[u] = true;
-        compSize++;
-        for (int v : G[u]) {
-            if (!visited[v]) {
-                if (dfs(v, u, G, visited, compSize))
-                    return true;
-            }
-            else if (v != parent) {
-                // found a back‐edge → cycle
-                return true;
-            }
+    bool dfs(int i, int p, vector<vector<int>> &adj, vector<bool> &vis, int &cnt) {
+        cnt++;
+        vis[i] = true;
+        bool ans = false;
+        for(auto &j : adj[i]) {
+            if(j == p) continue;
+            if(vis[j]) ans = true;
+            else ans |= dfs(j, i, adj, vis, cnt);
         }
-        return false;
+        return ans;
     }
-
-    ll maxScore(int n, vector<vector<int>>& edges) {
-        // 1) Build adjacency
-        vector<vector<int>> G(n);
-        for (auto &e : edges) {
-            G[e[0]].push_back(e[1]);
-            G[e[1]].push_back(e[0]);
+    
+    long long maxScore(long long n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        for(auto &v : edges) {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        
+        vector<bool> vis(n, false);
+        vector<int> cycles, chains;
+        for(int i = 0; i < n; i++) {
+            if(vis[i]) continue;
+            int cnt = 0;
+            if(dfs(i, -1, adj, vis, cnt)) cycles.push_back(cnt);
+            else chains.push_back(cnt);
         }
 
-        // 2) Classify components
-        vector<bool> visited(n, false);
-        vector<int> acyclic, cyclic;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                int sz = 0;
-                bool isCycle = dfs(i, -1, G, visited, sz);
-                (isCycle ? cyclic : acyclic).push_back(sz);
+        sort(cycles.rbegin(), cycles.rend());
+        sort(chains.rbegin(), chains.rend());
+
+        long long ans = 0;
+        for(auto x : cycles) {
+            ans += n*(n-1 + n-2);
+            n -= 3; x -= 3;
+            while(x) {
+                ans += n*(n+2);
+                x--; n--;
+            }
+            ans += (n+1)*(n+2);
+        }
+        for(auto x : chains) {
+            if(x == 1) break;
+            if(x == 2) {
+                ans += n*(n-1);
+                n -= 2;
+                continue;
+            }
+            ans += n*(n-1 + n-2);
+            n -= 3; x -= 3;
+            while(x) {
+                ans += n*(n+2);
+                x--; n--;
             }
         }
-
-        // 3) Sort increasing: acyclic first, then cyclic
-        sort(acyclic.begin(), acyclic.end());
-        sort(cyclic.begin(), cyclic.end());
-
-        ll total = 0;
-        int nextLabel = 1;
-
-        // Helper to assign one component of size sz,
-        // compute its contribution; 'makeCycle' toggles wrap-around.
-        auto processComp = [&](int sz, bool makeCycle) {
-            vector<ll> a(sz);
-            // assign labels nextLabel .. nextLabel+sz-1 symmetrically
-            int low = nextLabel, high = nextLabel + sz - 1;
-            for (int i = 0; i < sz/2; i++) {
-                a[i]          = low++;
-                a[sz-1-i]    = low++;
-            }
-            if (sz % 2) {
-                a[sz/2] = low++;
-            }
-            // accumulate edge-products
-            for (int i = 1; i < sz; i++) {
-                total += a[i] * a[i-1];
-            }
-            if (makeCycle && sz > 1) {
-                total += a[0] * a[sz-1];
-            }
-            nextLabel += sz;
-        };
-
-        // 4) Process all acyclic, then all cyclic
-        for (int sz : acyclic)
-            processComp(sz, /*makeCycle=*/false);
-        for (int sz : cyclic)
-            processComp(sz, /*makeCycle=*/true);
-
-        return total;
+        return ans;
     }
 };
