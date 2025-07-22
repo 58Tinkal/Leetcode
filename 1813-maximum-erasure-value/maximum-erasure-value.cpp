1b487873f6ffda12d5d904fc1eef804327187690
Tinkal kumar <tinkal300@gmail.com>
2025-07-22 21:48:56 +0530

    Time: 193 ms (36.43%) | Memory: 131 MB (43.04%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int s = 0, e = 0;
         int n = nums.size();
-        map<int, int> m;
-        vector<int> pre(n + 1, 0);
-        for (int i = 1; i <= n; i++) {
-            pre[i] = pre[i - 1] + nums[i-1];
-        }
+        int s = 0, e = 0;
+        int sum = 0, maxSum = 0;
+        unordered_set<int> seen;
 
-        int ans = INT_MIN;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            if (m.find(nums[i]) != m.end() && m[nums[i]] >= e) {
-                int idx = m[nums[i]];
-                s = idx;
-                ans = max(ans, sum);
-                sum -= (pre[idx + 1] - pre[e]);
-                e = idx + 1;
-                sum += nums[i];
-                m[nums[i]] = i;
+        while (e < n) {
+            if (seen.count(nums[e]) == 0) {
+                sum += nums[e];
+                seen.insert(nums[e]);
+                maxSum = max(maxSum, sum);
+                e++;
             } else {
-                m[nums[i]] = i;
-                sum += nums[i];
+                // remove nums[s] from window
+                seen.erase(nums[s]);
+                sum -= nums[s];
+                s++;
             }
         }
-        ans = max(ans, sum);
 
-        return ans;
+        return maxSum;
     }
 };
