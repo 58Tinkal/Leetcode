eb1783d8f4b6416cfa361dab58123ee09c6cb54d
Tinkal kumar <tinkal300@gmail.com>
2025-07-26 15:22:53 +0530

    Time: 789 ms (6.78%) | Memory: 432.9 MB (5.09%) - LeetSync

--- 3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.cpp
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.cpp
@@ -1,47 +1,37 @@
-#define ALL(x) begin(x), end(x)
 class Solution {
 public:
     long long maxSubarrays(int n, vector<vector<int>>& conflictingPairs) {
         long long valid = 0;
 
-        
-        vector<vector<int>> conflictingPoints(n+1);
-        //conflictingPoints[i] = {points which confliuct with i}
+        map<int, vector<int>> m;
 
-        for(auto &p : conflictingPairs) { //O(C)
-            //(a, b)
-            int a = min(p[0], p[1]);
-            int b = max(p[0], p[1]);
-
-            conflictingPoints[b].push_back(a); //{1,2,a,,,,b}
+        for (auto& it : conflictingPairs) {
+            int a = min(it[0], it[1]);
+            int b = max(it[0], it[1]);
+            m[b].push_back(a);
         }
 
-        int maxConflict = 0;
-        int secondMaxConflict = 0;
 
-        vector<long long> extra(n+1, 0);
-        //extra[i] = number of extra subarrays by removing the conflicting point i
+ int firstmaxConflict = 0;
+            int secondmaxConflict = 0;
+        vector<long long> extra(n + 1, 0);
 
-        //O(n+P)
-        //O(n)
-        for(int end = 1; end <= n; end++) { //visiting each point and treating them as subarray ending at this point
-            //total subarrays ending at this point 'end'
-            
-            //O(P)
-            for(int &u : conflictingPoints[end]) { //check all conflicting points of end
-                if(u >= maxConflict) {
-                    secondMaxConflict = maxConflict;
-                    maxConflict = u;
-                } else if(u > secondMaxConflict) {
-                    secondMaxConflict = u;
+        for (int i = 1; i <= n; i++) {
+           
+
+            for (auto& it : m[i]) {
+                if (it >= firstmaxConflict) {
+                    secondmaxConflict = firstmaxConflict;
+                    firstmaxConflict = it;
+                } else if (it > secondmaxConflict) {
+                    secondmaxConflict = it;
                 }
             }
 
-            valid += end - maxConflict;
-            extra[maxConflict] += maxConflict - secondMaxConflict;
+            valid += i - firstmaxConflict;
+            extra[firstmaxConflict] += firstmaxConflict - secondmaxConflict;
         }
 
-
-        return valid + *max_element(ALL(extra)); //you could also write valid + *max_element(begin(extra), end(extra))
+        return valid + *max_element(extra.begin(), extra.end());
     }
 };
