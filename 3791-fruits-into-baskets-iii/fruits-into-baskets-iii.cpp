ca31a2c4002e07776baf4550f5d78c8c9774abb3
Tinkal kumar <tinkal300@gmail.com>
2025-08-07 18:57:26 +0530

    Time: 70 ms (72.71%) | Memory: 189.2 MB (30.80%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
@@ -1,37 +1,44 @@
 class Solution {
+    vector<int> segTree;
+
+    void buildSegTreeRMQ(vector<int>& baskets,int low,int high,int st_idx){
+        if(low==high){
+            segTree[st_idx] = baskets[low];
+            return;
+        }
+
+        int mid = low + (high-low)/2;
+        buildSegTreeRMQ(baskets,low,mid,2*st_idx);
+        buildSegTreeRMQ(baskets,mid+1,high,2*st_idx+1);
+        segTree[st_idx] = max(segTree[2*st_idx],segTree[2*st_idx+1]);
+    }
+    int findLeftmostBasket(const int& fruit,int low,int high,int st_idx){
+        if(segTree[st_idx]<fruit)
+            return -1;
+        if(low==high){
+            segTree[st_idx] = -1;
+            return 1;
+        }
+        
+        int val;
+        int mid = low + (high-low)/2;
+        if(segTree[2*st_idx]>=fruit)    val = findLeftmostBasket(fruit,low,mid,2*st_idx);
+        else                            val = findLeftmostBasket(fruit,mid+1,high,2*st_idx+1);
+        segTree[st_idx] = max(segTree[2*st_idx],segTree[2*st_idx+1]);
+        return val;
+    }
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        int n = baskets.size();
-        int N = 1;
-        while (N <= n) N <<= 1;
+        int n = fruits.size();
+        segTree = vector<int>(4*n+1);
 
-        vector<int> segTree(N << 1);
-        for (int i = 0; i < n; ++i)
-            segTree[N + i] = baskets[i];
+        int st_idx = 1;
+        buildSegTreeRMQ(baskets,0,n-1,st_idx);
         
-        for (int i = N - 1; i > 0; --i)
-            segTree[i] = max(segTree[2 * i], segTree[2 * i + 1]);
-
         int count = 0;
-        for (int fruit : fruits) {
-            int index = 1;
-            if (segTree[index] < fruit) {
+        for(int i=0;i<n;++i){
+            if(findLeftmostBasket(fruits[i],0,n-1,st_idx)==-1)
                 count++;
-                continue;
-            }
-
-            while (index < N) {
-                if (segTree[2 * index] >= fruit)
-                    index = 2 * index;
-                else
-                    index = 2 * index + 1;
-            }
-
-            segTree[index] = -1;
-            while (index > 1) {
-                index >>= 1;
-                segTree[index] = max(segTree[2 * index], segTree[2 * index + 1]);
-            }
         }
         return count;
     }
