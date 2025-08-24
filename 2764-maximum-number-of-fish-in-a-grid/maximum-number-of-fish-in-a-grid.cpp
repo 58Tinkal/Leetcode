53572cda9221613b532a034a498c2eaeafde9647
Tinkal kumar <tinkal300@gmail.com>
2025-08-24 19:04:56 +0530

    Time: 7 ms (67.30%) | Memory: 96.4 MB (66.20%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -3,9 +3,9 @@ public:
     bool isValid(int nr, int nc, int m, int n) {
         return nr >= 0 && nc >= 0 && nr < m && nc < n;
     }
-    int dfs(vector<vector<int>>& grid, vector<vector<bool>>& vis, int m, int n,
-            int x, int y) {
-        int cnt = grid[x][y];
+    void dfs(vector<vector<int>>& grid, vector<vector<bool>>& vis, int m, int n,
+             int x, int y, int& cnt) {
+        cnt += grid[x][y];
         int dr[] = {-1, 1, 0, 0};
         int dc[] = {0, 0, -1, 1};
         for (int i = 0; i < 4; i++) {
@@ -13,10 +13,9 @@ public:
             int nc = y + dc[i];
             if (isValid(nr, nc, m, n) && !vis[nr][nc] && grid[nr][nc] > 0) {
                 vis[nr][nc] = true;
-                cnt += dfs(grid, vis, m, n, nr, nc);
+                dfs(grid, vis, m, n, nr, nc, cnt);
             }
         }
-        return cnt;
     }
     int findMaxFish(vector<vector<int>>& grid) {
         int m = grid.size();
@@ -25,9 +24,11 @@ public:
         int ans = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                int cnt = 0;
                 if (!vis[i][j] && grid[i][j] > 0) {
                     vis[i][j] = true;
-                    ans = max(ans, dfs(grid, vis, m, n, i, j));
+                    dfs(grid, vis, m, n, i, j, cnt);
+                    ans = max(ans, cnt);
                 }
             }
         }
