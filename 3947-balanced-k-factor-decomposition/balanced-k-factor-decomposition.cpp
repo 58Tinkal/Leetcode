93c2eab0f9c018451f58ca6468c214394f953e89
Tinkal kumar <tinkal300@gmail.com>
2025-08-31 14:59:44 +0530

    Time: 35 ms (51.21%) | Memory: 15.1 MB (26.23%) - LeetSync

---
+++ 3947-balanced-k-factor-decomposition/balanced-k-factor-decomposition.cpp
@@ -0,0 +1,56 @@
+class Solution {
+private:
+    vector<int> div;
+    vector<int> ans;
+    int val = 1e9;
+
+private:
+    void solve(int n, int k, int last, vector<int>& cur) {
+
+        if (k == 0) {
+            vector<int> temp = cur;
+            int x = temp[temp.size() - 1] - temp[0];
+            if (x <= val) {
+                x = val;
+                ans = temp;
+            }
+            return;
+        }
+
+        if (k == 1) {
+            if (n < last)
+                return;
+            cur.push_back(n);
+            solve(1, 0, n, cur);
+            cur.pop_back();
+        }
+
+        int idx = lower_bound(div.begin(), div.end(), last) - div.begin();
+        for (int i = idx; i < div.size(); i++) {
+            if (n % div[i] != 0)
+                continue;
+            cur.push_back(div[i]);
+            solve(n / div[i], k - 1, div[i], cur);
+            cur.pop_back();
+        }
+    }
+
+public:
+    vector<int> minDifference(int n, int k) {
+
+        for (int i = 1; i * i <= n; i++) {
+            if (i * i == n) {
+                div.push_back(i);
+                continue;
+            }
+            if (n % i == 0) {
+                div.push_back(i);
+                div.push_back(n / i);
+            }
+        }
+        sort(div.begin(), div.end());
+        vector<int> cur = {};
+        solve(n, k, 0, cur);
+        return ans;
+    }
+};
