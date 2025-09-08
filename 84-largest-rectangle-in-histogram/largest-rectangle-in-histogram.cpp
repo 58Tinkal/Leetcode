cb36b3669823213c315c751b0b1cdc88a32c4c4b
Tinkal kumar <tinkal300@gmail.com>
2025-09-09 00:32:32 +0530

    Time: 22 ms (61.69%) | Memory: 81.4 MB (74.58%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -8,14 +8,18 @@ public:
         for(int i = 0; i < n; i++) {
             while(!st.empty() && h[st.top()] > h[i]) {
                 int el = st.top();
+               // cout<<el<<' ';
                 st.pop();
                 int nse = i; 
                 int pse = (st.empty()) ? -1 : st.top(); 
+                //cout<<nse<<' '<<pse<<' ';
                 ans = max(ans, h[el] * (nse - pse - 1));
+              //  cout<<ans<<endl;
             }
+           // cout<<ans<<' ';
             st.push(i);
         }
-        
+        cout<<endl;
      
         while (!st.empty()) {
             int el = st.top();
@@ -23,6 +27,7 @@ public:
             int nse = n; 
             int pse = (st.empty()) ? -1 : st.top();
             ans = max(ans, h[el] * (nse - pse - 1));
+         //   cout<<ans<<endl;
         }
 
         return ans;
