a35b015086ab39ff29c78deab48aa697b0ddeae4
Tinkal kumar <tinkal300@gmail.com>
2025-09-18 18:37:15 +0530

    Time: 1021 ms (23.17%) | Memory: 348.9 MB (13.13%) - LeetSync

---
+++ 3680-count-connected-components-in-lcm-graph/count-connected-components-in-lcm-graph.cpp
@@ -0,0 +1,73 @@
+struct DSU {
+    vector<int> parent, sz;
+    DSU(int n) {
+        parent.resize(n);
+        sz.assign(n, 1);
+        iota(parent.begin(), parent.end(), 0);
+    }
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+    void unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b)
+            return;
+        if (sz[a] < sz[b]) {
+            parent[a] = b;
+            sz[b] += sz[a];
+        } else {
+            parent[b] = a;
+            sz[a] += sz[b];
+        }
+    }
+};
+class Solution {
+public:
+    int countComponents(vector<int>& nums, int threshold) {
+        int n = nums.size();
+        unordered_map<int, int> m;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= threshold) {
+                m[nums[i]] = i;
+            }
+        }
+        DSU dsu(n);
+
+        vector<vector<int>> divisors(threshold + 1);
+        for (auto& p : m) {
+            int val = p.first;
+            int idx = p.second;
+            for (int d = val; d <= threshold; d += val) {
+                divisors[d].push_back(idx);
+            }
+        }
+
+        for (int d = 1; d <= threshold; ++d) {
+            auto& vec = divisors[d];
+            if (vec.size() <= 1)
+                continue;
+            int base = vec[0];
+            for (size_t k = 1; k < vec.size(); ++k) {
+                dsu.unite(base, vec[k]);
+            }
+        }
+        
+        vector<char> seen(n, 0);
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > threshold) {
+                ++ans;
+            } else {
+                int r = dsu.find(i);
+                if (!seen[r]) {
+                    seen[r] = 1;
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
