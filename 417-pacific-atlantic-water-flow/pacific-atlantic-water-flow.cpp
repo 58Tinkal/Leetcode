b90375d837d5363548e8e78af4a602c5f3d91fa6
Tinkal kumar <tinkal300@gmail.com>
2025-10-05 12:00:36 +0530

    Time: 7 ms (62.44%) | Memory: 24.1 MB (38.65%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,45 +1,66 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& vis, vector<vector<int>>& heights, int r,
-             int c) {
-        vis[r][c] = 1;
-        int n = heights.size(), m = heights[0].size();
-        int dr[] = {0, 0, 1, -1};
-        int dc[] = {1, -1, 0, 0};
-        for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i], nc = c + dc[i];
-            if (nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc] &&
-                heights[nr][nc] >= heights[r][c]) {
-                dfs(vis, heights, nr, nc);
+    bool isValid(int r, int c, int n, int m) {
+        return r >= 0 && c >= 0 && r < n && c < m;
+    }
+
+    void bfs(vector<vector<int>>& grid, vector<vector<int>>& vis,
+             queue<pair<int, int>>& q) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int dr[] = {0, 1, 0, -1};
+        int dc[] = {1, 0, -1, 0};
+
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (isValid(nr, nc, n, m) && !vis[nr][nc] &&
+                    grid[nr][nc] >= grid[r][c]) {
+                    vis[nr][nc] = 1;
+                    q.push({nr, nc});
+                }
             }
         }
     }
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int n = heights.size(), m = heights[0].size();
-        vector<vector<int>> visP(n, vector<int>(m, 0));
-        vector<vector<int>> visA(n, vector<int>(m, 0));
-        for (int i = 0; i < m; i++) {
-            if (!visP[0][i])
-                dfs(visP, heights, 0, i);
-        }
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> pacific(n, vector<int>(m, 0));
+        vector<vector<int>> atlantic(n, vector<int>(m, 0));
+
+        queue<pair<int, int>> q;
+
         for (int i = 0; i < n; i++) {
-            if (!visP[i][0])
-                dfs(visP, heights, i, 0);
+            q.push({i, 0});
+            pacific[i][0] = 1;
         }
-        for (int i = 0; i < m; i++) {
-            if (!visA[n - 1][i])
-                dfs(visA, heights, n - 1, i);
+        for (int j = 1; j < m; j++) {
+            q.push({0, j});
+            pacific[0][j] = 1;
         }
+        bfs(mat, pacific, q);
+
+        while (!q.empty()) q.pop();
         for (int i = 0; i < n; i++) {
-            if (!visA[i][m - 1])
-                dfs(visA, heights, i, m - 1);
+            q.push({i, m - 1});
+            atlantic[i][m - 1] = 1;
         }
+        for (int j = 0; j < m; j++) {
+            q.push({n - 1, j});
+            atlantic[n - 1][j] = 1;
+        }
+        bfs(mat, atlantic, q);
+
         vector<vector<int>> ans;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (visP[i][j] && visA[i][j]) {
+                if (pacific[i][j] && atlantic[i][j])
                     ans.push_back({i, j});
-                }
             }
         }
         return ans;
