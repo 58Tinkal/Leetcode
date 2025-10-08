b1250467428ffa566309269ca1e782b6b3aa078e
Tinkal kumar <tinkal300@gmail.com>
2025-10-08 22:41:57 +0530

    Time: 32 ms (96.07%) | Memory: 134.3 MB (88.72%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2,
+                                       int k) {
         vector<vector<int>> ans;
         int n = nums1.size(), m = nums2.size();
-        if (n == 0 || m == 0) return ans;
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
 
-        using pii = pair<int, pair<int, int>>; // sum, {i, j}
-        priority_queue<pii, vector<pii>, greater<pii>> pq;
-
-        // Start with first element of nums2 for each nums1[i]
         for (int i = 0; i < min(n, k); i++) {
             pq.push({nums1[i] + nums2[0], {i, 0}});
         }
 
         while (!pq.empty() && k-- > 0) {
-            auto [sum, indices] = pq.top();
+            auto it = pq.top();
             pq.pop();
-            int i = indices.first;
-            int j = indices.second;
+            int sum = it.first;
+            int i = it.second.first;
+            int j = it.second.second;
 
             ans.push_back({nums1[i], nums2[j]});
 
-            // Move next in nums2 for same nums1[i]
             if (j + 1 < m) {
                 pq.push({nums1[i] + nums2[j + 1], {i, j + 1}});
             }
