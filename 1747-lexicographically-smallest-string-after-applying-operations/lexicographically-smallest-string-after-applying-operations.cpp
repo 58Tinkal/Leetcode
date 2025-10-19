9d160089fa96159004a5db31b5f3ff42d67651ca
Tinkal kumar <tinkal300@gmail.com>
2025-10-19 19:36:34 +0530

    Time: 334 ms (27.43%) | Memory: 90.8 MB (50.88%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
+    string smallest;
+
     void rotate(string& s, int b) {
         reverse(s.begin(), s.end());
         reverse(s.begin(), s.begin() + b);
         reverse(s.begin() + b, s.end());
     }
-    string findLexSmallestString(string s, int a, int b) {
-        string smallest = s;
-        queue<string> q;
-        unordered_set<string> vis;
-        q.push(s);
-        vis.insert(s);
-        while (!q.empty()) {
-            string str = q.front();
-            q.pop();
-            if (str < smallest) {
-                smallest = str;
-            }
-            string temp = str;
-            for (int i = 1; i < temp.size(); i += 2) {
-                temp[i] = ((temp[i] - '0' + a) % 10) + '0';
-            }
-            if (!vis.count(temp)) {
-                q.push(temp);
-                vis.insert(temp);
-            }
-            rotate(str, b);
 
-            if (!vis.count(str)) {
-                q.push(str);
-                vis.insert(str);
-            }
-        }
+    void dfs(string s, int a, int b, unordered_set<string>& vis) {
+        if (vis.count(s))
+            return;
+        vis.insert(s);
+        if (s < smallest)
+            smallest = s;
+
+        string temp = s;
+        for (int i = 1; i < temp.size(); i += 2)
+            temp[i] = ((temp[i] - '0' + a) % 10) + '0';
+        dfs(temp, a, b, vis);
+
+        string rotated = s;
+        rotate(rotated, b);
+        dfs(rotated, a, b, vis);
+    }
+
+    string findLexSmallestString(string s, int a, int b) {
+        unordered_set<string> vis;
+        smallest = s;
+        dfs(s, a, b, vis);
         return smallest;
     }
 };
