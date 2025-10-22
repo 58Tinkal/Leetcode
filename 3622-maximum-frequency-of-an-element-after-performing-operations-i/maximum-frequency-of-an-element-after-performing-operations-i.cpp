43bb6ab82d923d09a29bdd28964f1744b642eff6
Tinkal kumar <tinkal300@gmail.com>
2025-10-22 23:02:37 +0530

    Time: 247 ms (44.88%) | Memory: 167.2 MB (31.46%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        int maxVal = *max_element(nums.begin(), nums.end()) + k + 2;
-        int* count = new int[maxVal]();
+        int maxVal = *max_element(begin(nums), end(nums)) + k;
 
-        for (int v : nums)
-            count[v]++;
+        vector<int> freqArr(maxVal + 2, 0);
+        unordered_map<int, int> m;
 
-        for (int i = 1; i < maxVal; i++)
-            count[i] += count[i - 1];
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
 
-        int res = 0;
-        for (int i = 0; i < maxVal; i++) {
-            int left = max(0, i - k);
-            int right = min(maxVal - 1, i + k);
-            int total = count[right] - (left ? count[left - 1] : 0);
-            int freq = count[i] - (i ? count[i - 1] : 0);
-            res = max(res, freq + min(numOperations, total - freq));
+            int l = max(nums[i] - k, 0);
+            int r = min(nums[i] + k, maxVal);
+
+            freqArr[l]++;
+            freqArr[r + 1]--;
         }
 
-        return res;
+        int ans = 1;
+
+        for (int i = 0; i <= maxVal; i++) {
+            if (i > 0)
+                freqArr[i] += freqArr[i - 1];
+
+            int originalCount = m[i];
+            int needConversion = freqArr[i] - originalCount;
+            int res = min(needConversion, numOperations);
+            ans = max(ans, originalCount + res);
+        }
+
+        return ans;
     }
 };
