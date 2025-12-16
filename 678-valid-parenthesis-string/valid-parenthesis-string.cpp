f551b26db28309a560a8f86c39f9abb0bca492b1
Tinkal kumar <tinkal300@gmail.com>
2025-12-16 19:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (47.37%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int dp[105][105];
-    bool solve(int idx, int c, string& s) {
-        if (c < 0)
-            return false;
-        if (idx == s.size()) {
-            if (c == 0)
-                return true;
-            return false;
-        }
-        if (dp[idx][c] != -1)
-            return dp[idx][c];
-        if (s[idx] == '(')
-            return dp[idx][c] = solve(idx + 1, c + 1, s);
-        if (s[idx] == ')')
-            return dp[idx][c] = solve(idx + 1, c - 1, s);
-        return dp[idx][c] = solve(idx + 1, c + 1, s) or
-                            solve(idx + 1, c - 1, s) or solve(idx + 1, c, s);
-    }
     bool checkValidString(string s) {
-        int n = s.size();
-        int c = 0;
-        memset(dp, -1, sizeof(dp));
-        return solve(0, c, s);
+        int mn = 0, mx = 0;
+        for (auto& it : s) {
+            if (it == '(') {
+                mn += 1;
+                mx += 1;
+            } else if (it == ')') {
+                mx -= 1;
+                mn -= 1;
+            } else {
+                mn -= 1;
+                mx += 1;
+            }
+            if (mn < 0)
+                mn = 0;
+            if (mx < 0)
+                return false;
+        }
+        if (mn == 0)
+            return true;
+        return false;
     }
 };
