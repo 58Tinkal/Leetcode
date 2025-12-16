90083d47a05a7a3db2f877b0ba0bcbfc1f716f53
Tinkal kumar <tinkal300@gmail.com>
2025-12-16 22:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (84.39%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int ans = 0;
+        int farthest = 0;
+        int current  = 0;
+        for(int i=0;i<nums.size()-1;i++){
+            farthest = max(farthest, i + nums[i]);
+            if(i == current){
+                current = farthest;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
